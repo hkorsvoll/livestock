de8676d556ada1591e51169ec7a873c51e95c85b
Håvard Korsvoll <korsvoll@gmail.com>
2013-08-02 21:33:10 +0200

    Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index,
+                                        :destroy, :following, :followers]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Brukar er sletta."
+    redirect_to users_url
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Velkomen til husdyrdatabasen!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile oppdatert"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+
+  private
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+end
