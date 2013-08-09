48a92fb507fcf9ae1b020a10be2d3e7f49b983c5
Håvard Korsvoll <korsvoll@gmail.com>
2013-08-09 22:18:35 +0200

    Adding Owner To User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @owners_any = Owner.any?
   end
 
   def new
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,27 @@ describe "User pages" do
     it {should have_selector('li', text: myuser.email)}
     it {should have_link(myuser.owner.name, href: owner_path(myuser.owner))}
     it {should have_link('Edit', href: edit_user_path(myuser))}
+    it {should have_link('Owners', href: owners_path)}
+    it {should_not have_link('Create Owner', href: new_owner_path)}
+
+    describe "when no owner exist" do
+      before do
+        Owner.delete_all
+        visit user_path(myuser)
+      end
+      it {should_not have_link('Owners'), href: owners_path}
+      it {should have_link('Create Owner', href: new_owner_path)}
+    end
+
+    describe "when no owner is assosiated with user" do
+      before do
+        myuser.owners.delete_all
+        visit user_path(myuser)
+      end
+      it {should have_selector('h3', text: 'No owners assosiated')}
+      it {should have_link('Owners'), href: owners_path}
+      it {should have_link('Create Owner', href: new_owner_path)}
+    end
 
   end
 end
